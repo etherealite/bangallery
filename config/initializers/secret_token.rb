2230bc7346d49ca546386471f6c3e52d1fe0670d
Etherealite <evanbanghm@yahoo.com>
2012-02-05 18:31:44 -0800

    ity bitty t*ty committe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bangallery::Application.config.secret_token = 'fcd0e9894dd7907bfe26f346936aa7bf50d756a6160bbfb4455304555e363f6abd474d28b1050abfbd62864ab3f58b7918248c133c5e94cb16e868a9ca30a8d9'
