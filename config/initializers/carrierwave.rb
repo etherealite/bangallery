6e41f22441319a0fb06947df615d758ce4de6cfa
Etherealite <evanbanghm@yahoo.com>
2012-02-09 01:07:25 -0800

    adding google storage initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,13 @@
-if Rails.env.test? or Rails.env.development?
-  CarrierWave.configure do |config|
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider                         => 'Google',
+      :google_storage_access_key_id     => ENV['GDEVSTORE_ID'],
+      :google_storage_secret_access_key => ENV['GDEVSTORE_SECRET']
+    }
+    config.fog_directory = 'bangallery'
+  else
     config.storage = :file
   end
 end
