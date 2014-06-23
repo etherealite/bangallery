3712051cd62b8b805951a6310a35e06b2e6472a0
etherealite <seclayer@gmail.com>
2014-06-23 02:24:11 +0000

    belongs in previous commit but i'm lazy

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -110,7 +110,7 @@ ActiveAdmin.setup do |config|
   # roots for each namespace.
   #
   # Default:
-  # config.root_to = 'dashboard#index'
+   config.root_to = 'images#index'
 
 
   # == Admin Comments
@@ -118,7 +118,7 @@ ActiveAdmin.setup do |config|
   # This allows your users to comment on any resource registered with Active Admin.
   #
   # You can completely disable comments:
-  # config.allow_comments = false
+  config.allow_comments = false
   #
   # You can disable the menu item for the comments index page:
   # config.show_comments_in_menu = false
@@ -210,7 +210,12 @@ ActiveAdmin.setup do |config|
   #     admin.download_links = [:xml, :pdf]
   #
   #   end
-
+     config.namespace :admin do |admin|
+  
+       # Disable the links entirely
+       admin.download_links = false
+  
+     end
 
   # == Pagination
   #
