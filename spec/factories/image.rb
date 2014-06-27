0c823cf3e8f63d3c7ad7951da7f7339b27bdff7a
etherealite <seclayer@gmail.com>
2014-06-27 05:19:15 +0000

    image factory now fixed

--- spec/factories/image.rb
+++ spec/factories/image.rb
@@ -1,13 +1,20 @@
 
 FactoryGirl.define do
-imagesource = '/spec/support/images'
-images = Dir[imagesource]
+  imagesource = '/spec/support/images'
+  imagesource = File.join(Rails.root, imagesource)
+  imagesource = Dir.new(imagesource)
+  images = []
+  imagesource.each do |file|
+      next if File.directory? file
+      images.push(File.join(imagesource, file))
+  end
+
+  puts images
   factory :image do
     title Faker::Lorem.word
     description Faker::Lorem.paragraph
     file {
-        Rails.logger.info imagesource
-        File.open(File.join(Rails.root, "/#{imagesource}/#{images.sample}")) 
+        File.open(images.sample)
     }
   end
 end
